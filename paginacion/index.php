76b19b7e105f7c4272b087740d721bc8bc2b6f51
Alejandro Vera <alejandroverah97@gmail.com>
2020-12-03 14:59:47 -0500

    Calculos de  paginacion para mostrar x  resultados

--- paginacion/index.php
+++ paginacion/index.php
@@ -11,7 +11,15 @@ try{
     //juego de caracteres 
     $base->exec("SET CHARACTER SET UTF8");
 
-    //sentencia SQL que devuelve registros de BBDD
+    //variable para almacenar cuantos registrros queremos guardar por pagina
+    $tamagno_paginas=3;
+
+    //Variable para mostrar el numero de pagina donde nos encontramos
+    $pagina=1;
+
+    $empezar_desde=($pagina-1)*$tamagno_paginas;
+
+    //sentencia SQL que devuelve registros de BBDD. Saber cuantos regisros nos devuelve la consulta
     $sql_total="SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS WHERE SECCIÓN='DEPORTES'";
 
     //Sentencia preparada que devuelve todos los registros selecionados
@@ -19,17 +27,39 @@ try{
 
     $resultado->execute(array());
 
+    //cuantos rergistros nos devuelve consulta sql
+
+    $num_filas=$resultado->rowCount(); //cuenta filas que tengo dentro del array
+
+    //averiguar cuantas paginas va a tener nuestra paginacion
+    $total_paginas=ceil($num_filas/$tamagno_paginas); //evuelve redonder el numero la funcion CEIL
+
+    echo "Numero de registros de la consulta: " . $num_filas . "<br>";
+    echo "Mostramos " . $tamagno_paginas . " registros por pagina <br>";
+    echo "Mostrando la pagina " . $pagina . " de " . $total_paginas . "<br>";
+
     //Recorrer array almacenado
-    while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+    //while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
 
-        echo "Nombre Artículo: " . $registro["NOMBREARTÍCULO"] . " Sección: " . $registro["SECCIÓN"] . " Precio: " . $registro["PRECIO"] . " País de origen: " . $registro["PAÍSDEORIGEN"] . "<br>";
+        //echo "Nombre Artículo: " . $registro["NOMBREARTÍCULO"] . " Sección: " . $registro["SECCIÓN"] . " Precio: " . $registro["PRECIO"] . " País de origen: " . $registro["PAÍSDEORIGEN"] . "<br>";
 
-    }
-
+   // } 
 
     //cerrar cursor
     $resultado->closeCursor();
 
+    //almacenar instruccion sql con el limite. 2da consulta para ue nos lo muestre en pantalla
+    $sql_limite="SELECT NOMBREARTÍCULO, SECCIÓN, PRECIO, PAÍSDEORIGEN FROM PRODUCTOS WHERE SECCIÓN='DEPORTES' LIMIT $empezar_desde, $tamagno_paginas";
+    $resultado=$base->prepare($sql_limite);
+
+    $resultado->execute(array());
+    
+    while($registro=$resultado->fetch(PDO::FETCH_ASSOC)){
+
+        echo "Nombre Artículo: " . $registro["NOMBREARTÍCULO"] . " Sección: " . $registro["SECCIÓN"] . " Precio: " . $registro["PRECIO"] . " País de origen: " . $registro["PAÍSDEORIGEN"] . "<br>";
+
+    } 
+    
 } catch (Exception $e) {
 
     //concatena con getLine para darnos linea de error
